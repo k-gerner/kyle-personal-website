acfed04c88e617ff67d205c6dc3670bda4bbd8bd
Kyle Gerner <kgcoltsfan@gmail.com>
2025-10-26 17:18:45 -0400

    smaller max width for pop up on small screens

--- frontend/src/components/PopUp.tsx
+++ frontend/src/components/PopUp.tsx
@@ -13,7 +13,7 @@ const PopUp: React.FC<PopUpProps> = ({
 }) => {
     return createPortal(
         <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
-            <div className="bg-background-base rounded-lg shadow-lg p-8 max-w-xl w-full relative max-h-[90vh] overflow-y-auto">
+            <div className="bg-background-base rounded-lg shadow-lg p-8 w-full relative max-h-[90vh] max-w-[95vw] md:max-w-xl overflow-y-auto">
                 <button
                     className="absolute rounded-full top-2 right-2 text-xl font-bold text-danger bg-background-base hover:bg-danger hover:text-background-base transition-colors"
                     onClick={() => setShow(false)}
