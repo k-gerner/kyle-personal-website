c7412dc900cc6a978111769256751877b08e87cd
Kyle Gerner <kgcoltsfan@gmail.com>
2025-10-31 04:01:27 -0400

    remove safelist

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -128,7 +128,4 @@ module.exports = {
     },
   },
   plugins: [require("daisyui")],
-  safelist: [
-    { pattern: /./ }, // temporarily allow all classes to ensure DaisyUI isn't being purged
-  ],
 }
